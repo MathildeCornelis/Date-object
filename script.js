2314cd3bec7e555616278ca0b4576d220262b2e2
MathildeCornelis <mathilde.cornelis26@gmail.com>
2022-12-13 12:17:28 +0100

    ex 2

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 /*Exercice 1*/
+    let divUsa = document.getElementById('usa')
+    let divIceland = document.getElementById('iceland')
+    let divRussia = document.getElementById('russia')
+    let divBelgium = document.getElementById('belgium')
     // First, define the time zones of each city
     const timeAnchorage = 'America/Anchorage';
     const timeReykjavik = 'Atlantic/Reykjavik';
@@ -12,12 +16,24 @@
     const brusselsDate = new Date().toLocaleString('en-US', { timeZone: timeBrussels });
 
     // Finally, display the date and time for each city
-    console.log(`Anchorage: ${anchorageDate}`);
-    console.log(`Reykjavik: ${reykjavikDate}`);
-    console.log(`Saint-Petersburg: ${saintPetersburgDate}`);
-    console.log(`Brussels: ${brusselsDate}`);
+    divUsa.innerHTML = `Anchorage: ${anchorageDate}`;
+    divIceland.innerHTML = `Reykjavik: ${reykjavikDate}`;
+    divRussia.innerHTML = `Saint-Petersburg: ${saintPetersburgDate}`;
+    divBelgium.innerHTML = `Brussels: ${brusselsDate}`;
 
 /*Exercise 2*/
-let naissance = new Date ('1999-02-26');
-let now = new Date();
-console.log(Math.round((now.getTime() - naissance.getTime()) / (1000 * 60 * 60 * 24)) + ' days have passed betweeen my born and today');
+    let divDay = document.getElementById('day')
+    let naissance = new Date ('1999-02-26');
+    let now = new Date();
+    divDay.innerHTML = Math.round((now.getTime() - naissance.getTime()) / (1000 * 60 * 60 * 24)) + ' days have passed betweeen my born and today';
+    
+    let inputDiv = document.getElementById('date');
+    inputDiv.addEventListener('keyup', showDifTime)
+    function showDifTime(e) {
+        if (e.key === "Enter") {
+        let now = new Date();
+        let x = new Date(inputDiv.value);
+        console.log(x);
+        document.getElementById('reponse').innerHTML = Math.round((now.getTime() - x.getTime()) / (1000 * 60 * 60 * 24)) + ' day' + (Math.round((now.getTime() - x.getTime()) / (1000 * 60 * 60 * 24)) > 1 ? "s" : "") + ' have passed since ' + inputDiv.value + '.';
+        };
+    };
